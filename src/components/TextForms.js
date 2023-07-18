389a07d72af3d25d9375cb1c4abd0c5c3486f950
sejal175 <sejalaggarwal0@gmail.com>
2023-07-18 20:21:46 +0530

    onchange function

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TextForms() {
-
+export default function TextForms(props) {
+  //onchange function
+  const handleOnChnage = (event) => {
+    setText(event.target.value);
+  };
+    const [text, setText] = useState("");
   return (
     <>
       <div className="mb-3">
@@ -11,11 +15,14 @@ export default function TextForms() {
         <textarea
           className="form-control mt-3"
           placeholder="Type Something"
+          value={text}
           id="myBox"
           rows="12"
+          onChange={handleOnChnage}
         >
+           <p>{text.length>0?`${text}`: ""}</p>
         </textarea>
- 
+        
       </div>
     </>
   );
